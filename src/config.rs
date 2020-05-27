6afcb957a07a8d1479c84af146b39f8ba7d806f2
Børre Gaup <albbas@gmail.com>
2020-05-27 21:35:52 +0200

    Fix typo

--- src/config.rs
+++ src/config.rs
@@ -29,7 +29,7 @@ impl Config {
 
 fn path() -> PathBuf {
     let path = config_path();
-    log::info!("Conifg path: {:?}", path);
+    log::info!("Config path: {:?}", path);
     match path {
         Some(p) => p,
         None => panic!("Cannot read the config directory. We need to read our config file in your config directory."),
